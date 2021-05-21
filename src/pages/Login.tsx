7d5c0561ce3fcf7bb1ffce6c7a208b08beb923f2
Saleh Haddawi <saleh.haddawi@gmail.com>
2021-05-21 21:23:42 +0300

    tailwind

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function Login() {
+function App() {
     return (
         <div>
-            Login
+            HELLO
         </div>
     );
 }
 
-export default Login;
+export default App;
